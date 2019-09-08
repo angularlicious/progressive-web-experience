ddf0134d79871d636bd0f630df35cd9a7272e53b
Matt Vaughn <matt.vaughn@buildmotion.com>
2019-09-08 09:39:26 -0600

    display video(s) in cards

--- workspace/libs/lms/business/courses/src/lib/business/http-course-repository.service.ts
+++ workspace/libs/lms/business/courses/src/lib/business/http-course-repository.service.ts
@@ -31,6 +31,28 @@ export class HttpCourseRepositoryService extends ServiceBase {
     course.videoUrl = 'https://youtube.com/embed/E4IocEOK1II';
     courses.push(course);
 
+    const course2 = new VideoCourse();
+    course2.author = new Author('Matt Vaughn');
+    course2.author.bio = 'Loves tacos, jazz, and Angular. Building enterprise applications using CLEAN principles and patterns.';
+    course2.category = CourseCategory.Angular;
+    course2.description =
+      'A short video to reintroduce you to the game changing capabilities of the Angular Workspace. If you or your team needs to be better at sharing and reusing code - then this is for you';
+    course2.id = 1;
+    course2.title = 'Angular Workspace Reintroduction';
+    course2.videoUrl = 'https://youtube.com/embed/E4IocEOK1II';
+    courses.push(course2);
+
+    const course3 = new VideoCourse();
+    course3.author = new Author('Matt Vaughn');
+    course3.author.bio = 'Loves tacos, jazz, and Angular. Building enterprise applications using CLEAN principles and patterns.';
+    course3.category = CourseCategory.Angular;
+    course3.description =
+      'A short video to reintroduce you to the game changing capabilities of the Angular Workspace. If you or your team needs to be better at sharing and reusing code - then this is for you';
+    course3.id = 1;
+    course3.title = 'Angular Workspace Reintroduction';
+    course3.videoUrl = 'https://youtube.com/embed/E4IocEOK1II';
+    courses.push(course3);
+
     const apiResponse = new SuccessApiResponse();
     apiResponse.Data = courses;
     apiResponse.IsSuccess = true;
