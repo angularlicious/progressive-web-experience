c426374db7164cfb4705c5a7f808ca91933a1c18
Matt Vaughn <matt.vaughn@buildmotion.com>
2019-10-20 21:36:47 -0600

    component updates

--- workspace/libs/lms-feature/courses/src/lib/index/index.component.ts
+++ workspace/libs/lms-feature/courses/src/lib/index/index.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ComponentBase } from '@angularlicious/foundation';
+import { Router } from '@angular/router';
+import { LoggingService } from '@angularlicious/logging';
 
 @Component({
   selector: 'angularlicious-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
+// export class IndexComponent extends ComponentBase implements OnInit {
 export class IndexComponent implements OnInit {
-  constructor() {}
+  // constructor(loggingService: LoggingService, router: Router) {
+  //   super('IndexComponent', loggingService, router);
+  // }
 
   ngOnInit() {}
 }
