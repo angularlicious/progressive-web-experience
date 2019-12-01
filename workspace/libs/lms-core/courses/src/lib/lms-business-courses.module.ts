04fc66ff74e122fb7171f9be6eaaa2853f0c4779
Matt Vaughn <matt.vaughn@buildmotion.com>
2019-12-01 13:26:26 -0500

    json configuration for firebase

--- workspace/libs/lms-core/courses/src/lib/lms-business-courses.module.ts
+++ workspace/libs/lms-core/courses/src/lib/lms-business-courses.module.ts
@@ -6,23 +6,10 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirestoreCourseRepositoryService } from './business/firestore-course-repository.service';
-
-const firebaseOptions = {
-  apiKey: 'AIzaSyCcPPAdpUUuO3Kczc3LWyrYmwC8Ghxiwr0',
-  authDomain: 'angular-architecture-web.web.app',
-  databaseURL: 'https://angular-architecture-web.firebaseio.com',
-  projectId: 'angular-architecture-web',
-  storageBucket: 'angular-architecture-web.appspot.com',
-  messagingSenderId: '',
-};
+import * as firebaseOptionsConfig from './firebaseOptions.json';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    AngularFireModule.initializeApp(firebaseOptions),
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-  ],
+  imports: [CommonModule, AngularFireModule.initializeApp(firebaseOptionsConfig), AngularFireAuthModule, AngularFirestoreModule],
   exports: [],
   providers: [BusinessProviderService, FirestoreCourseRepositoryService, HttpService],
 })
