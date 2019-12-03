a0fd77bad2fc236611768e9f1ac62a69db9a7126
Matt Vaughn <matt.vaughn@buildmotion.com>
2019-12-03 00:37:37 -0500

    notifications implementation

---
+++ workspace/libs/notifications/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/notifications.module';
+export { NotificationsService } from './lib/notifications.service';
+export { Notification } from './lib/notification.model';
+export { NotificationType } from './lib/notification-type.enum';
